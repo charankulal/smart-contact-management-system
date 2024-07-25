da7ef0e85ac56d493cdf970f4b89ec15302f90b0
Charan <charankulal0241@gmail.com>
2024-07-25 18:59:16 +0530

    Created responsive and theme based navbar and other pages layouts.

--- src/main/java/com/charan/scms/controllers/PageControllers.java
+++ src/main/java/com/charan/scms/controllers/PageControllers.java
@@ -2,6 +2,7 @@ package com.charan.scms.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +22,19 @@ public class PageControllers {
     @RequestMapping("/services")
     public  String servicesPage(){
         return "services";
+    }
+    @GetMapping("/contact")
+    public  String contactPage(){
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public  String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public  String registerPage(){
+        return "register";
     }
 }
